81a3624218197da9a6a091e5de6b6ba0bdfb5921
Muskan <muskannarang1998@gmail.com>
2019-07-05 01:23:35 +0530

    Create Solution2.js
    Plus Minus

---
+++ Solution2.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+    var n = arr.length;
+    var p=0, g=0, z=0;
+    for (var i = 0; i < n; i++){
+        if (arr[i] > 0)
+            ++p;
+        else if (arr[i] < 0)
+            ++g;
+        else if (arr[i] == 0)
+            ++z;
+            
+    }
+    
+    process.stdout.write(""+ p/n+ "\n" +g/n+ "\n"+ z/n );
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
